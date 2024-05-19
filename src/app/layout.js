0f4e59c4d44ff94a00bd82185a1df7019dbd452f
Shruti Sonani <shrutisonani28@gmail.com>
2024-05-19 17:10:40 +0530

    Implement footer section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,17 @@
 import { Lato, Open_Sans } from "next/font/google";
-import '@/styles/styles.sass';
+import "@/styles/styles.sass";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
-const lato = Lato({ 
+const lato = Lato({
   subsets: ["latin"],
-  weight: ['400', '700', '900'],
-  variable: '--font-lato'
+  weight: ["400", "700", "900"],
+  variable: "--font-lato",
 });
 
-const openSans = Open_Sans({ 
+const openSans = Open_Sans({
   subsets: ["latin"],
-  variable: '--font-open-sans'
+  variable: "--font-open-sans",
 });
 
 export const metadata = {
@@ -20,9 +21,12 @@ export const metadata = {
 
 const RootLayout = ({ children }) => (
   <html lang="en">
-    <body className={`${openSans.className} ${openSans.variable} ${lato.variable}`}>
+    <body
+      className={`${openSans.className} ${openSans.variable} ${lato.variable}`}
+    >
       <Header />
       {children}
+      <Footer />
     </body>
   </html>
 );
