4a8152ec673730d408877560c6a4aa9068ac182b
Shruti Sonani <shrutisonani28@gmail.com>
2024-05-21 04:30:49 +0530

    Display blog dynamic

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,28 +1,37 @@
 import Button from "../button/button";
 import styles from "./card.module.sass";
+import ConditionalRender from "../conditional-render";
 
-const Card = () => (
+const Card = (props) => (
   <div className={styles.card_wrap}>
     <div className={styles.card}>
       {/* blog image wrapper */}
       <div className={styles.card_imageWrap}>
         <div className={styles.card_image}>
-          <img src="/blog-thumbnail.png" alt="thumbnail" />
+          <ConditionalRender condition={props.thumbnail}>
+            <img src={props.thumbnail} alt="thumbnail" />
+          </ConditionalRender>
         </div>
       </div>
       {/* blog content */}
       <div className={styles.card_content}>
-        <div className={`${styles.card_lable} h6 mb-10 c-orange`}>
-          Product Review
-        </div>
-        <div className={`${styles.card_title} h3 mb-20`}>
-          How to add featured images in wordpress
-        </div>
-        <p className={`${styles.card_summary} fw-600`}>
-          Framer Motion is a versatile animation library for React, known for
-          its simplicity and powerful animations.
-        </p>
-        <Button href="#">Read More</Button>
+        <ConditionalRender condition={props.category}>
+          <div className={`${styles.card_lable} h6 mb-10 c-orange`}>
+            {props.category}
+          </div>
+        </ConditionalRender>
+
+        <ConditionalRender condition={props.title}>
+          <div className={`${styles.card_title} h3 mb-20`}>{props.title}</div>
+        </ConditionalRender>
+
+        <ConditionalRender condition={props.summary}>
+          <p className={`${styles.card_summary} fw-600`}>{props.summary}</p>
+        </ConditionalRender>
+
+        <ConditionalRender condition={props.slug}>
+          <Button href={props.slug}>Read More</Button>
+        </ConditionalRender>
       </div>
     </div>
   </div>
---
+++ src/components/conditional-render.js
@@ -0,0 +1,7 @@
+const ConditionalRender = ({ condition, children }) => {
+  if (condition) return children;
+
+  return null;
+};
+
+export default ConditionalRender;
